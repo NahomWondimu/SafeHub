2f7df1f3113dc82a7c3d1dd7c026642fa35bd83d
nahomwondimu <nahomawt@gmail.com>
2025-04-12 11:50:20 -0400

    magic files jumped a branch

--- mySafeHub/NetworkAnalysis/models.py
+++ mySafeHub/NetworkAnalysis/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Network(models.Model):
+    placeHolder = models.CharField(max_length=255)
+    time_entered = models.TimeField(null=True)
--- mySafeHub/NetworkAnalysis/urls.py
+++ mySafeHub/NetworkAnalysis/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Net_Analysis, name='network_like_a_web'),
+    path('process/', views.process_data, name='process'),
 ]
